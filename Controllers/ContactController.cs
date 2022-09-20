b6b467fd0c971deb1473e59c0d9c35d84a505e00
valenricciramos <valenricciramos@gmail.com>
2022-09-20 13:55:49 -0300

    nuevo commit

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PriceGames.Models;
+
+namespace PriceGames.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        public static List<Contact> FakeContacts = new List<Contact>()
+            {
+                new Contact()
+                {
+                    Name = "Gero",
+                    CelularNumber = 3414121234,
+
+                },
+                new Contact()
+                {
+                    Name = "Dino",
+                    CelularNumber = 1234456789,
+ 
+                }
+            };
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(FakeContacts);
+        }
+
+        [HttpGet]
+        [Route("GetOne/{Id}")]
+        public IActionResult GetOneById(int CelularNumber)
+        {
+            return Ok(FakeContacts.Where(x => x.CelularNumber == CelularNumber).ToList());
+        }
+    }
+}
+
